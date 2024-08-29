454570ceed323d0d25c1c567d8bc18c10f418963
Eddy <edwardyue93@gmail.com>
2024-08-29 20:18:15 +0200

    feat: :sparkles: azure functions of ai to fastapi

---
+++ app/query.py
@@ -0,0 +1,43 @@
+import os
+from typing import List
+
+from llama_index.core import StorageContext
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.astra_db import AstraDBVectorStore
+
+
+def index_query(project_name: str, query: str, top_k: int, model: str) -> List[str]:
+    # Astra DB config
+    astra_endpoint = os.environ["ASTRA_DB_ENDPOINT"]
+    astra_token = os.environ["ASTRA_DB_TOKEN"]
+
+    if not astra_endpoint or not astra_token:
+        raise ValueError("Astra DB config not found")
+
+    embed_model = OpenAIEmbedding(
+        model=model
+    )
+
+    # Astra DB vector store
+    vector_store = AstraDBVectorStore(
+        token=astra_token,
+        api_endpoint=astra_endpoint,
+        collection_name=project_name,
+        # Dimensions: https://docs.datastax.com/en/astra-db-serverless/get-started/concepts.html
+        embedding_dimension=1536,
+    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=vector_store,
+        storage_context=storage_context,
+        embed_model=embed_model
+    )
+
+    retriever = index.as_retriever(
+        vector_store_query_mode="mmr",
+        similarity_top_k=top_k,
+        vector_store_kwargs={"mmr_prefetch_factor": 4}
+    )
+    nodes = retriever.retrieve(query)
+    return [node.get_content() for node in nodes]
---
+++ main.py
@@ -0,0 +1,69 @@
+from typing import Annotated
+from dotenv import load_dotenv
+from fastapi import FastAPI, File, Form, UploadFile
+import os
+import secrets
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import logging
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBasicCredentials
+from pydantic import BaseModel
+from app.embed import execute_embedding
+from app.query import index_query
+from llama_index.embeddings.openai import OpenAIEmbeddingModelType
+
+security = HTTPBasic()
+
+def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
+    user_name = os.getenv('BASIC_AUTH_USERNAME')
+    user_password = os.getenv('BASIC_AUTH_PASSWORD')
+
+    correct_username = secrets.compare_digest(credentials.username, user_name)
+    correct_password = secrets.compare_digest(credentials.password, user_password)
+    if not (correct_username and correct_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/health")
+def get_health():
+    return {"status": "up"}
+
+
+@app.post("/{project}/embed")
+async def create_file(
+    project: str,
+    file: Annotated[bytes, File()],
+    model: str = Form(OpenAIEmbeddingModelType.TEXT_EMBED_3_SMALL),
+):
+    content: str = file.decode("utf-8")
+    execute_embedding(content, project, model)
+
+    return { "success": True }
+
+class QueryRequest(BaseModel):
+    query: str
+    top_k: int
+    model: str = OpenAIEmbeddingModelType.TEXT_EMBED_3_SMALL
+
+@app.post("/{project}/query")
+async def post_project_query(project: str, req: QueryRequest, credentials: HTTPBasicCredentials = Depends(verify_credentials)):
+    try:
+        query = req.query
+        top_k = req.top_k
+        model = req.model
+
+        if query is None or top_k is None:
+            raise HTTPException(status_code=400, detail="Missing 'query' or 'top_k' or 'model' request body params")
+
+        chunks = index_query(project, query, top_k, model)
+
+        return chunks
+    except Exception as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+    
